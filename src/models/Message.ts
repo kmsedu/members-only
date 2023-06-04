a58d40142037c8e67f724dc4aab614eb8c6708af
kris <kmsedu@tuta.io>
2023-06-04 18:42:53 +1000

    Created draft models

---
+++ src/models/Message.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { Types } from "mongoose";
+import { User } from "./User";
+
+const Schema = mongoose.Schema;
+
+interface MessageInterface {
+  title: string;
+  timestamp: Date;
+  message: string;
+  author: Types.ObjectId;
+}
+
+const messageSchema = new Schema<MessageInterface>({
+  title: { type: String, require: true },
+  timestamp: { type: Date, default: new Date() },
+  message: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+const Message = mongoose.model<MessageInterface>("Message", messageSchema);
+
+export { Message };
