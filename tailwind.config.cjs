eb2f4ae7bff1aea7fd33f69e9d32222af9e5472f
kris <kmsedu@tuta.io>
2023-06-10 15:07:36 +1000

    Swapped out fonts for google CDN fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['"Roboto Flex"', "sans-serif"],
-                title: ["Martel Sans", "sans-serif"],
+                sans: ['"Fira Sans"', "sans-serif"],
             },
         },
     },
